f47ff192a2b34a21f0e8f82158e4232a9373cfb2
TR3BBY <lincoln.treble@gmail.com>
2026-09-04 15:18:04 +0100

    Create the Footer & Nav. Added appropriate styling

--- Dr-pepper-Frontend/src/Components/Footer/Footer.jsx
+++ Dr-pepper-Frontend/src/Components/Footer/Footer.jsx
@@ -1,8 +1,50 @@
 import '../../Components/Footer/Footer.css'
 
-function MainFooter () {
-    return(
-        <></>
+import { NavLink } from 'react-router-dom'
+
+import Logo from "../../assets/Dr-pepper-logo.png";
+
+function MainFooter() {
+    return (
+        <footer className="footer">
+            <div className="footer-top">
+                <img src={Logo} alt="Dr Pepper Logo" className="footer-logo" />
+
+                <div className="footer-links">
+                    <div className="footer-column">
+                        <h3>Home</h3>
+                        <NavLink to="/">What is Dr Pepper</NavLink>
+                        <NavLink to="/products">Check our Products</NavLink>
+                    </div>
+
+                    <div className="footer-column">
+                        <h3>About</h3>
+                        <NavLink to="/about">Who are Dr Pepper</NavLink>
+                        <NavLink to="/about">Origin Story</NavLink>
+                        <NavLink to="/about">Where to find us</NavLink>
+                    </div>
+
+                    <div className="footer-column">
+                        <h3>Products</h3>
+                        <NavLink to="/products">Our Products</NavLink>
+                        <NavLink to="/products">Reviews</NavLink>
+                    </div>
+                </div>
+
+                <div className="footer-social">
+                    <i className="ri-instagram-fill"></i>
+                    <i className="ri-facebook-fill"></i>
+                    <i className="ri-youtube-fill"></i>
+                    <i className="ri-tiktok-fill"></i>
+                    <i className="ri-twitter-x-fill"></i>
+                </div>
+            </div>
+
+            <div className="footer-bottom">
+                <p className="text-left-bottom">Dr Pepper is a registered trademark of Dr Pepper/Seven Up, Inc.</p>
+                <p>©2026 Dr Pepper/Seven Up, Inc.</p>
+            </div>
+        </footer>
     )
 }
 
--- Dr-pepper-Frontend/src/Components/Header/Header.jsx
+++ Dr-pepper-Frontend/src/Components/Header/Header.jsx
@@ -1,8 +1,40 @@
 import '../../Components/Header/Header.css'
 
+import { NavLink } from 'react-router-dom'
+
+import Logo from '../../assets/Dr-pepper-logo.png'
+
 function Header () {
     return(
-        <></>
+
+        <nav className='nav-bar'>
+            <div className='nav-container'>
+                <div className='left-section'>
+                    <NavLink to = "/" >
+                        <img src = {Logo} className='brand-logo' />
+                    </NavLink>
+                </div>
+
+                <div className='right-section'>
+                    <ul className='nav-links'>
+
+                        <li className='nav-link'>
+                            <NavLink to = "/">Home</NavLink>
+                        </li>
+
+                        <li className='nav-link'>
+                            <NavLink to = "/about">About</NavLink>
+                        </li>
+
+                        <li className='nav-link'>
+                            <NavLink to = "/products">Products</NavLink>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+            
+        </nav>
     )
 }
 
